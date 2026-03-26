86a494da367992df848b2f13342247edaeb9738f
DESKTOP-D5DFVFJ\karth <karthiksai.0812@gmail.com>
2026-03-26 18:42:36 +0530

    feat(infrastructure): add DapperContext for DB connections & add ConnectionString in appsettings.json

---
+++ WSC.Delivery/WSC.Delivery.Infrastructure/Persistence/Context/DapperContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace WSC.Delivery.Infrastructure.Persistence.Context
+{
+    public class DapperContext
+    {
+        private readonly string _conString;
+        private readonly IConfiguration _config;
+
+        public DapperContext(string conString, IConfiguration config)
+        {
+            _conString = _config.GetConnectionString("DefaultConnection");
+            _config = config;
+        }
+        public IDbConnection CreateConnection() => new SqlConnection(_conString);
+    }
+}
