d0811b598a772cb44f8602e7bc6ef2dc5b49ff47
DESKTOP-D5DFVFJ\karth <karthiksai.0812@gmail.com>
2026-04-06 10:42:08 +0530

    feat(crm) : add FluentValidator for CreateOpportunityDto and UpdateOpportunityDto

---
+++ WSC.CRM/WSC.CRM.Application/Validators/OpportunityValidators/UpdateOpportunityValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WSC.CRM.Application.Dtos;
+
+namespace WSC.CRM.Application.Validators.OpportunityValidators
+{
+    public  class UpdateOpportunityValidator : AbstractValidator<UpdateOpportunityDto>
+    {
+        public UpdateOpportunityValidator() 
+        {
+            RuleFor(x => x.OpportunityId)
+                .GreaterThan(0).WithMessage("Opportunity ID must be a positive integer.");
+
+            RuleFor(x => x.OpportunityName)
+                .MaximumLength(100).WithMessage("Opportunity name cannot exceed 100 characters.");
+
+            RuleFor(x => x.Stage)
+                .IsInEnum().WithMessage("Invalid opportunity stage.");
+
+            RuleFor(x => x.Amount)
+                .GreaterThanOrEqualTo(0).WithMessage("Amount must be a positive value.");
+
+        }
+    }
+}
