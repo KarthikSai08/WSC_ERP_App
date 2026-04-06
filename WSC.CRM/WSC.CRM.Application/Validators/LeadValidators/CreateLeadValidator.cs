af795eaeb42146b5f176ed62f3e3529af7956421
DESKTOP-D5DFVFJ\karth <karthiksai.0812@gmail.com>
2026-04-06 10:51:15 +0530

    feat(crm) : add FluentValidator for CreateLeadDto and UpdateLeadDto

---
+++ WSC.CRM/WSC.CRM.Application/Validators/LeadValidators/CreateLeadValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WSC.CRM.Application.Dtos;
+
+namespace WSC.CRM.Application.Validators.LeadValidators
+{
+    public  class CreateLeadValidator : AbstractValidator<CreateLeadDto>
+    {
+        public CreateLeadValidator() 
+        {
+            RuleFor(x => x.LeadName)
+                .NotEmpty().WithMessage("Lead name is required.")
+                .MaximumLength(100).WithMessage("Lead name cannot exceed 100 characters.");
+
+            RuleFor(x => x.LeadEmail)
+                .NotEmpty().WithMessage("Lead email is required.")
+                .EmailAddress().WithMessage("Invalid email format.");
+
+            RuleFor(x => x.LeadPhone)
+                .NotEmpty().WithMessage("Lead phone number is required.")
+                .Matches(@"^\+?[1-9]\d{1,14}$").WithMessage("Invalid phone number format.");
+
+            RuleFor(x => x.CustomerId)
+                .GreaterThan(0).WithMessage("Customer ID must be greater than zero.");
+        }
+    }
+}
---
+++ WSC.CRM/WSC.CRM.Application/Validators/LeadValidators/UpdateLeadValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WSC.CRM.Application.Dtos;
+
+namespace WSC.CRM.Application.Validators.LeadValidators
+{
+    public class UpdateLeadValidator : AbstractValidator<UpdateLeadDto>
+    {
+        public UpdateLeadValidator()
+        {
+            RuleFor(x => x.LeadId)
+                .GreaterThan(0).WithMessage("Lead ID must be greater than zero.");
+
+            RuleFor(x => x.LeadName)
+                .MaximumLength(100).WithMessage("Lead name cannot exceed 100 characters.");
+
+            RuleFor(x => x.LeadEmail)
+                .EmailAddress().WithMessage("Invalid email format.");
+
+            RuleFor(x => x.LeadPhone)
+                .Matches(@"^\+?[1-9]\d{1,14}$").WithMessage("Invalid phone number format.");
+        }
+    }
+}
