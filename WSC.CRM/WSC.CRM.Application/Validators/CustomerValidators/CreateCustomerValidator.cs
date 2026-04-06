b99e7bf05b31f54c8be04cf38e7fe6c7eb610997
DESKTOP-D5DFVFJ\karth <karthiksai.0812@gmail.com>
2026-04-06 11:19:57 +0530

    feat(crm) : add FluentValidation for CreateCustomerDto and UpdateCustomerDto

---
+++ WSC.CRM/WSC.CRM.Application/Validators/CustomerValidators/CreateCustomerValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WSC.CRM.Application.Dtos;
+
+namespace WSC.CRM.Application.Validators.CustomerValidators
+{
+    public  class CreateCustomerValidator : AbstractValidator<CreateCustomerDto>
+    {
+        public CreateCustomerValidator()
+        {
+            RuleFor(x => x.CxName)
+                .NotEmpty().WithMessage("Customer name is required.")
+                .MaximumLength(100).WithMessage("Customer name cannot exceed 100 characters.");
+
+            RuleFor(x => x.CxEmail)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email format.");
+
+            RuleFor(x => x.CxPhone)
+                .NotEmpty().WithMessage("Phone number is required.")
+                .Matches(@"^\+?[1-9]\d{1,14}$").WithMessage("Invalid phone number format.");
+        }
+    }
+}
