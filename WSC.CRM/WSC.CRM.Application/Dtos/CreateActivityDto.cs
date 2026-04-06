89c92e6f76bc315e366c69c76b79653d0a865cf7
DESKTOP-D5DFVFJ\karth <karthiksai.0812@gmail.com>
2026-04-06 11:12:25 +0530

    feat(crm) : add FluentValidation for CreateActivityDto and UpdateActivityDto

--- WSC.CRM/WSC.CRM.Application/Dtos/CreateActivityDto.cs
+++ WSC.CRM/WSC.CRM.Application/Dtos/CreateActivityDto.cs
@@ -8,7 +8,7 @@ namespace WSC.CRM.Application.Dtos
     public class CreateActivityDto
     {
         public string Title { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public ActivityType Type { get; set; }
         public DateTime ScheduledAt { get; set; }
         public int LeadId { get; set; }
---
+++ WSC.CRM/WSC.CRM.Application/Validators/ActivityValidators/CreateActivityValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WSC.CRM.Application.Dtos;
+
+namespace WSC.CRM.Application.Validators.ActivityValidators
+{
+    public class CreateActivityValidator : AbstractValidator<CreateActivityDto>
+    {
+        public CreateActivityValidator() 
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Activity name is required.")
+                .MaximumLength(100).WithMessage("Activity name cannot exceed 100 characters.");
+
+            RuleFor(x => x.LeadId)
+                .GreaterThan(0)
+                .WithMessage("Valid Lead ID is required.");
+
+            RuleFor(x => x.Type)
+                .IsInEnum()
+                .WithMessage("Invalid activity type.");
+        }
+    }
+}
