276358bd7d0b7a56adf1821135d81f3e94b1b3ac
DESKTOP-D5DFVFJ\karth <karthiksai.0812@gmail.com>
2026-04-06 13:09:12 +0530

    feat(store) : add FluentValidation for CreateOrderDto and UpdateOrderDto

---
+++ WSC.Store/WSC.Store.Application/Validators/OrderValidator/CreateOrderValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WSC.Store.Application.Dtos;
+
+namespace WSC.Store.Application.Validators.OrderValidator
+{
+    public class CreateOrderValidator : AbstractValidator<CreateOrderDto>
+    {
+        public CreateOrderValidator() 
+        {
+            RuleFor(x => x.CustomerId)
+                .GreaterThan(0).WithMessage("CustomerId must be greater than 0.");
+
+            RuleFor(x => x.TotalAmount)
+                .GreaterThan(0).WithMessage("TotalAmount must be greater than 0.");
+
+            RuleFor(x => x.Status)
+                .IsInEnum().WithMessage("Status must be a valid enum value.");
+
+        }
+    }
+}
---
+++ WSC.Store/WSC.Store.Application/Validators/OrderValidator/UpdateOrderValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WSC.Store.Application.Dtos;
+
+namespace WSC.Store.Application.Validators.OrderValidator
+{
+    public class UpdateOrderValidator : AbstractValidator<UpdateOrderDto>
+    {
+        public UpdateOrderValidator() 
+        {
+            RuleFor(x => x.OrderId)
+                .GreaterThan(0).WithMessage("OrderId must be greater than 0.");
+
+            RuleFor(x => x.CustomerId)
+                .GreaterThan(0).WithMessage("CustomerId must be greater than 0.");
+
+            RuleFor(x => x.TotalAmount)
+                .GreaterThan(0).WithMessage("TotalAmount must be greater than 0.");
+
+            RuleFor(x => x.Status)
+                .IsInEnum().WithMessage("Status must be a valid enum value.");
+        }
+    }
+}
