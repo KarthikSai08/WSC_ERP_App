57ca53ef007eb90d00aee9ae08abddd9e5568dfc
DESKTOP-D5DFVFJ\karth <karthiksai.0812@gmail.com>
2026-04-08 11:24:21 +0530

    feat(shared) : add InSufficientException

---
+++ WSC.Shared/WSC.Shared.Contracts/Exceptions/InSufficientException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WSC.Shared.Contracts.Exceptions
+{
+    public class InSufficientException : Exception
+    {
+        public InSufficientException(string entity, int stock) : base($"InSufficient Stock for {entity} only has {stock} Nos. Please try again") { }
+
+        public InSufficientException() : base("Insufficient Stock!") { }
+    }
+}
