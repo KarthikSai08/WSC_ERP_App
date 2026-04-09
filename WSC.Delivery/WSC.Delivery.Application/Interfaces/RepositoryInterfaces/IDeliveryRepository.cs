307784457856656c25c4062e23b59ec0ddb2cd8c
DESKTOP-D5DFVFJ\karth <karthiksai.0812@gmail.com>
2026-04-09 15:32:14 +0530

    feat(delivery) : add repository interfaces for Delivery, DeliveryAgent, DeliveryItem, DeliveryTracking

---
+++ WSC.Delivery/WSC.Delivery.Application/Interfaces/RepositoryInterfaces/IDeliveryRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WSC.Delivery.Domain.Entities;
+using WSC.Shared.Contracts.Dtos.DeliveryLayer;
+namespace WSC.Delivery.Application.Interfaces.RepositoryInterfaces
+{
+    public interface IDeliveryRepository
+    {
+        Task<bool> CreateDeliveryAsync(OrderDelivery delivery, CancellationToken ct);
+        Task<IEnumerable<OrderDeliveryResponseDto>> GetAllDeliveriesAsync(CancellationToken ct);
+        Task<OrderDeliveryResponseDto> GetByDeliveryIdAsync(int deliveryId, CancellationToken ct);
+        Task<OrderDelivery> GetDeliveryEntityByIdAsync(int deliveryId, CancellationToken ct);
+        Task<bool> DeleteDeliveryByIdAsync(int deliveryId, CancellationToken ct);
+        Task<OrderDelivery> GetByDeliveryStatusAsync(string sts, CancellationToken ct);
+        Task<bool> UpdateDeliveryDetailsAsync(OrderDelivery delivery, CancellationToken ct);
+        Task<bool> UpdateDeliveryStatusAsync(string sts, CancellationToken ct);
+
+
+
+    }
+}
