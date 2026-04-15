d39703cb389c582c2cfa366bb4d660d09e857455
DESKTOP-D5DFVFJ\karth <karthiksai.0812@gmail.com>
2026-04-15 10:13:51 +0530

    feat(common) : add
    -DeliveryItemDisplayDto
    -ProductDisplayDto
    -TrackingDisplayDto

---
+++ WSC.Dashboard/WSC.Dashboard.Application/Dtos/DeliveryItemDisplayDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WSC.Dashboard.Application.Dtos
+{
+    public class DeliveryItemDisplayDto
+    {
+        public int DeliveryItemId { get; set; }
+        public int Quantity { get; set; }
+        public ProductDisplayDto Product { get; set; } = null!;
+    }
+}
---
+++ WSC.Dashboard/WSC.Dashboard.Application/Dtos/TrackingDisplayDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WSC.Delivery.Domain.Enums;
+
+namespace WSC.Dashboard.Application.Dtos
+{
+    public class TrackingDisplayDto
+    {
+        public int TrackingId { get; set; }
+        public int DeliveryId { get; set; }
+        public DeliveryStatus Status { get; set; }
+        public string Location { get; set; }
+        public string Remarks { get; set; }
+    }
+}
