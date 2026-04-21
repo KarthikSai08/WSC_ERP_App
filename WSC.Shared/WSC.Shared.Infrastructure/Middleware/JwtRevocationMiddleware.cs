3485022bda656bbcdd4482bc3b5952c9960b7584
DESKTOP-D5DFVFJ\karth <karthiksai.0812@gmail.com>
2026-04-21 17:40:45 +0530

    feat(shared) : add JwtMiddleware

---
+++ WSC.Shared/WSC.Shared.Infrastructure/Middleware/JwtRevocationMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using WSC.Shared.Contracts.Interfaces.JwtInterfaces;
+
+namespace WSC.Shared.Infrastructure.Middleware
+{
+    public class JwtRevocationMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public JwtRevocationMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, IJwtBlocklistService blocklistService)
+        {
+
+            if (context.User.Identity?.IsAuthenticated == true)
+            {
+                var jti = context.User.FindFirst(JwtRegisteredClaimNames.Jti)?.Value;
+                if (!string.IsNullOrEmpty(jti) && await blocklistService.IsBlockedAsync(jti))
+                {
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    context.Response.ContentType = "application/json";
+                    await context.Response.WriteAsync("{\"success\": false, \"message\": \"Token has been revoked. Please Login again\"}");
+                    return;
+                }
+            }
+            await _next(context);
+        }
+    }
+}
