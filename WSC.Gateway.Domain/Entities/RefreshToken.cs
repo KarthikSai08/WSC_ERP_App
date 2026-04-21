bc73eb657a6484783268f3320410c24e37583f10
DESKTOP-D5DFVFJ\karth <karthiksai.0812@gmail.com>
2026-04-21 14:00:40 +0530

    feat(gateway) : add entities

---
+++ WSC.Gateway.Domain/Entities/RefreshToken.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WSC.Gateway.Domain.Entities
+{
+    public class RefreshToken
+    {
+        public int TokenId { get; set; }
+        public int UserId { get; set; }
+        public string Token { get; set; } = null!;
+        public DateTime ExpiresAt { get; set; }
+        public bool IsRevoked { get; set; } 
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? RevokedAt { get; set; }
+        public string? ReplacedByToken { get; set; }
+
+        public bool IsExpired => DateTime.UtcNow >= ExpiresAt;
+        public bool IsActive => !IsRevoked && !IsExpired;
+
+
+    }
+}
---
+++ WSC.Gateway.Domain/Entities/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WSC.Gateway.Domain.Entities
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Role { get; set; } = null!;
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; }
+        public DateTime LastLoginAt { get; set; }
+    }
+}
