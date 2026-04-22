6c414abb81f02a5800efbe2ed3ccc2d423821ae4
DESKTOP-D5DFVFJ\karth <karthiksai.0812@gmail.com>
2026-04-22 16:40:54 +0530

    feat(gateway) : add BearerTokenHandler

---
+++ WSC.Gateway.Infrastructure/Http/BearerTokenHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace WSC.Gateway.Infrastructure.Http
+{
+    public class BearerTokenHandler : DelegatingHandler
+    {
+        private readonly IHttpContextAccessor _http;
+        public BearerTokenHandler(IHttpContextAccessor http)
+        {
+            _http = http;
+        }
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var token = _http.HttpContext?.Request.Headers["Authorization"].FirstOrDefault();
+
+            if (!string.IsNullOrEmpty(token))
+            {
+                var rawToken = token.StartsWith("Bearer ", StringComparison.OrdinalIgnoreCase)
+                    ? token["Bearer ".Length..]
+                    : token;
+
+                request.Headers.Authorization =
+                    new AuthenticationHeaderValue("Bearer", rawToken);
+            }
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
