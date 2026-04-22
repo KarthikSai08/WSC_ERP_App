07d0ffadc9d87296515a0a9592e03019e5ddd5c7
DESKTOP-D5DFVFJ\karth <karthiksai.0812@gmail.com>
2026-04-22 16:41:39 +0530

    feat(gateway) : add dapperContext
    -add packages
    -add references

--- WSC.Gateway.Infrastructure/Persistence/DapperContext.cs
+++ WSC.Gateway.Infrastructure/Persistence/DapperContext.cs
@@ -1,6 +1,19 @@
-﻿namespace WSC.Gateway.Infrastructure.Persistence
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace WSC.Gateway.Infrastructure.Persistence
 {
-    internal class DapperContext
+    public class DapperContext
     {
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("DefaultConnection") ?? throw new ArgumentNullException("DefaultConnection");
+        }
+
+        public IDbConnection CreateConnection()
+            => new SqlConnection(_connectionString);
     }
 }
