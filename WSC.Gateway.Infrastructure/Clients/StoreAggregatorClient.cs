21f350d2ff405cf231d3d21090fe726539352e19
DESKTOP-D5DFVFJ\karth <karthiksai.0812@gmail.com>
2026-04-23 17:58:48 +0530

    Create StoreAggregatorClient.cs

---
+++ WSC.Gateway.Infrastructure/Clients/StoreAggregatorClient.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+using WSC.Gateway.Application.Interfaces.Clients;
+using WSC.Shared.Contracts.Common;
+using WSC.Shared.Contracts.Dtos.StoreLayer;
+
+namespace WSC.Gateway.Infrastructure.Clients
+{
+    public class StoreAggregatorClient : IStoreAggregatorClient
+    {
+        private readonly HttpClient _http;
+        private static readonly JsonSerializerOptions _options = new()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+        public async Task<IEnumerable<OrderResponseDto>?> GetAllOrdersAsync(CancellationToken ct)
+            => await GetAsync<IEnumerable<OrderResponseDto>>("api/Orders/all-orders", ct);
+
+        public async Task<IEnumerable<ProductResponseDto>?> GetAllProductsAsync(CancellationToken ct)
+            => await GetAsync<IEnumerable<ProductResponseDto>>("api/Produts/all-products", ct);
+
+        private async Task<T?> GetAsync<T>(string url, CancellationToken ct)
+        {
+            var response = await _http.GetAsync(url, ct);
+            if (!response.IsSuccessStatusCode) return default;
+            var content = await response.Content.ReadAsStringAsync(ct);
+            var api = JsonSerializer.Deserialize<ApiResponse<T>>(content, _options);
+            return api != null && api.Success ? api.Data : default;
+        }
+    }
+}
