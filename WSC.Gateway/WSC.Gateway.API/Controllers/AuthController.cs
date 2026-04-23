16f5cd0762a52c57f6c59f36749dec903f22a233
DESKTOP-D5DFVFJ\karth <karthiksai.0812@gmail.com>
2026-04-23 18:01:11 +0530

    feat(gateway) : add authController
    - config Program.cs

---
+++ WSC.Gateway/WSC.Gateway.API/Controllers/AuthController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using WSC.Gateway.Application.Dtos.AuthDtos;
+using WSC.Gateway.Application.Interfaces;
+using WSC.Gateway.Domain.Entities;
+using WSC.Shared.Contracts.Common;
+
+namespace WSC.Gateway.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _service;
+        public AuthController(IAuthService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("login")]
+        [AllowAnonymous]
+        public async Task<ActionResult<ApiResponse<LoginResponseDto>>> LoginAsync([FromBody] LoginRequestDto dto, CancellationToken ct)
+        {
+            var result = await _service.LoginAsync(dto, ct);
+            if (result.Success)
+                return Ok(result);
+            return Unauthorized(result);
+        }
+
+        [HttpPost("refresh")]
+        [AllowAnonymous]
+        public async Task<ActionResult<ApiResponse<LoginResponseDto>>> RefreshTokenAsync([FromBody] RefreshTokenRequestDto dto, CancellationToken ct)
+        {
+            var result = await _service.RefreshTokenAsync(dto, ct);
+            if (result.Success)
+                return Ok(result);
+            return Unauthorized(result);
+        }
+
+        [HttpPost("logout")]
+        [Authorize]
+        public async Task<ActionResult<ApiResponse<bool>>> LogoutAsync([FromBody] RefreshTokenRequestDto dto, CancellationToken ct)
+        {
+            var accessToken = HttpContext.Request.Headers["Authorization"]
+                .FirstOrDefault()?.Replace("Bearer ", "") ?? string.Empty;
+
+            var result = await _service.LogoutAsync(accessToken, dto.RefreshToken, ct);
+            return Ok(result);
+        }
+        [HttpPost("register")]
+        [AllowAnonymous]
+        public async Task<ActionResult<ApiResponse<int>>> RegisterAsync([FromBody] RegisterRequestDto dto, CancellationToken ct)
+        {
+            var result = await _service.RegisterAsync(dto, ct);
+            if (result.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+
+        [HttpGet("me")]
+        [Authorize]
+        public ActionResult<ApiResponse<object>> Me()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var email = User.FindFirst(ClaimTypes.Email)?.Value;
+            var name = User.FindFirst(ClaimTypes.Name)?.Value;
+            var role = User.FindFirst(ClaimTypes.Role)?.Value;
+
+            return Ok(ApiResponse<object>.Ok(new { userId, email, name, role }, "Token Valid"));
+
+        }
+    }
+}
--- WSC.Gateway/WSC.Gateway.API/Program.cs
+++ WSC.Gateway/WSC.Gateway.API/Program.cs
@@ -1,32 +1,93 @@
-using WSC.Shared.Contracts.Interfaces.CRMClients;
-using WSC.Shared.Infrastructure.Clients;
+using Scalar.AspNetCore;
+using Serilog;
+using StackExchange.Redis;
+using System.Text.Json.Serialization;
+using WSC.Gateway.API.Filters;
+using WSC.Gateway.API.Middleware;
+using WSC.Gateway.Application.DependencyInjection;
+using WSC.Gateway.Application.Interfaces.Clients;
+using WSC.Gateway.Infrastructure.Clients;
+using WSC.Gateway.Infrastructure.DependencyInjection;
+using WSC.Gateway.Infrastructure.Http;
+
+using WSC.Shared.Infrastructure.Extensions;
+using WSC.Shared.Infrastructure.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers(opts =>
+{
+    opts.Filters.Add<ValidationFilter>();
+})
+.AddJsonOptions(opts =>
+{
+    opts.JsonSerializerOptions.Converters
+    .Add(new JsonStringEnumConverter());
+});
+builder.Services.AddScoped<ValidationFilter>();
+
 builder.Services.AddOpenApi();
+builder.Host.ConfigureSerilog();
 
+builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+{
+    var config = builder.Configuration["Redis:ConnectionString"]
+        ?? throw new InvalidOperationException("Redis:ConnectionString not configured");
+    return ConnectionMultiplexer.Connect(config);
+});
 
-var crmUrl = builder.Configuration["Services:CRM"];
+builder.Services.AddJwtAuthentication(builder.Configuration);
 
-builder.Services.AddHttpClient<ICustomerClient, CustomerClient>(client =>
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddGatewayApplicationServices();
+builder.Services.AddGatewayInfrastructureService();
+
+var crmUrl = builder.Configuration["Services:CRM"]
+    ?? throw new InvalidOperationException("Services:CRM is not configured");
+var storeUrl = builder.Configuration["Services:Store"]
+    ?? throw new InvalidOperationException("Services:Store is not Configured");
+var deliveryUrl = builder.Configuration["Services:Delivery"]
+    ?? throw new InvalidOperationException("Services:Delivery is  not Configured");
+
+builder.Services.AddHttpClient<ICrmAggregatorClient, CrmAggregatorClient>(client =>
 {
     client.BaseAddress = new Uri(crmUrl);
-});
+    client.Timeout = TimeSpan.FromSeconds(10);
+})
+.AddHttpMessageHandler<BearerTokenHandler>();
+
+
+builder.Services.AddHttpClient<IStoreAggregatorClient, StoreAggregatorClient>(client =>
+{
+    client.BaseAddress = new Uri(storeUrl);
+    client.Timeout = TimeSpan.FromSeconds(10);
+})
+.AddHttpMessageHandler<BearerTokenHandler>();
+
+builder.Services.AddHttpClient<IDeliveryAggregatorClient, DeliveryAggregatorClient>(client =>
+{
+    client.BaseAddress = new Uri(deliveryUrl);
+    client.Timeout = TimeSpan.FromSeconds(10);
+})
+.AddHttpMessageHandler<BearerTokenHandler>();
 
 var app = builder.Build();
 
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseMiddleware<CorrelationMiddleware>();
+app.UseSerilogRequestLogging();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
